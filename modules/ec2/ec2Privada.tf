dd52b17cfdd0ad0031b42f368aaa9bd8beb7e5ab
Kauan-deAlmeida <kauan.almeida@sptech.school>
2025-04-16 19:37:48 -0300

    feature: Adicionando Instancia privada para banco, adicionando arquivo txt com comandos terraform

--- modules/ec2/ec2Privada.tf
+++ modules/ec2/ec2Privada.tf
@@ -18,3 +18,24 @@ resource "aws_instance" "ec2_privada" {
 output "private_instance_id" {
   value = aws_instance.ec2_privada.id
 }
+
+resource "aws_instance" "ec2_privada_banco_01" {
+  ami                    = "ami-084568db4383264d4" 
+  instance_type          = "t2.micro"
+  subnet_id              = var.private_subnet_id
+  key_name               = var.key_name
+  vpc_security_group_ids = [var.vpc_security_group_id]
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp2"
+  }
+
+  tags = {
+    Name = "ec2-privada-terabite-banco-01"
+  }
+}
+
+output "private_instance_banco_01_id" {
+  value = aws_instance.ec2_privada_banco_01.id
+}
